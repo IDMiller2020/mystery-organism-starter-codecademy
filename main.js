dec9b15709bf42a47d38ca0f3469770f54fbd31e
Scott <76458930+IDMiller2020@users.noreply.github.com>
2021-08-23 11:07:58 -0600

    factory and mutation method completed

--- main.js
+++ main.js
@@ -13,10 +13,35 @@ const mockUpStrand = () => {
   return newStrand;
 };
 
+const pAequorFactory = (num, dnaArr) => {
+  return {
+    specimenNum: num,
+    specimenDna: dnaArr,
+    mutate() {
+      const randBase = Math.floor(Math.random() * 15)
+      let newBase = ''
+      do {
+        newBase = returnRandBase()
+      } while (dnaArr[randBase] === newBase)
+      dnaArr[randBase] = newBase
+      return dnaArr
+    }
+  }
+}
 
 
+// Testing section:
 
-
+// console.log(pAequorFactory(101, ['A','A','A','A','A','A','A','A','A','A','A','A','A','A','A']))
+let testSpecimen1 = pAequorFactory(101, ['A','A','A','A','A','A','A','A','A','A','A','A','A','A','A'])
+let testSpecimen2 = pAequorFactory(102, mockUpStrand())
+console.log(`testSpecimen1.specimenNum: ${testSpecimen1.specimenNum}`)
+console.log(`testSpecimen1.specimenDna: ${testSpecimen1.specimenDna}`)
+console.log(`testSpecimen1.mutate: ${testSpecimen1.mutate()}`)
+console.log(`testSpecimen1 mutated DNA: ${testSpecimen1.specimenDna}`)
+console.log(`testSpecimen2.specimenNum: ${testSpecimen2.specimenNum}`)
+console.log(`testSpecimen2.specimenDna: ${testSpecimen2.specimenDna}`)
+console.log(`testSpecimen2.mutate: ${testSpecimen2.mutate()}`)
 
 
 
